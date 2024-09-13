d64c1bfc7728b81a6a17d5ad943186442804bb59
Ezequiel <ezegonzalez800@gmail.com>
2024-09-13 13:40:15 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'inawin',
+  webDir: 'www'
+};
+
+export default config;
