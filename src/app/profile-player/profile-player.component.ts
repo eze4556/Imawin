28cadafd8fe95560cbcb8c55d42e98435aa4aced
Ezequiel <ezegonzalez800@gmail.com>
2024-11-12 10:20:23 -0300

    profileplayer-filtros

--- src/app/profile-player/profile-player.component.ts
+++ src/app/profile-player/profile-player.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; // Importa el servicio Router
+import { PlayerProfile } from 'src/models/playerProfile.model';
 import { FirestoreService } from 'src/services/firestore.service';
 
 
@@ -12,9 +13,26 @@ export class ProfilePlayerComponent implements OnInit {
 
    players: any[] = [];
 
+
+    // Variables para los filtros
+  tipoJugador: string = 'jugador';
+  piernaHabil: string = 'ambos';
+  posicion: string = 'todos';
+  edad: number = 20;
+  altura: number = 170;
+  experiencia: number = 10;
+  buscador: string = '';
+  pais: string = '';
+  provincia: string = '';
+  ciudad: string = '';
+
+    jugadoresFiltrados: any[] = []; // Lista de jugadores después del filtro
+
+
+
+
   constructor(private router: Router,private firestoreService: FirestoreService) { }
 
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit() {
 
         this.getAllPlayers();
@@ -26,18 +44,52 @@ export class ProfilePlayerComponent implements OnInit {
   }
 
 
- getAllPlayers() {
-  this.firestoreService.getPlayers().subscribe(data => {
-    console.log("Datos de jugadores obtenidos:", data); // Verificar si los datos llegan aquí
-    if (data && data.length > 0) {
+getAllPlayers() {
+    this.firestoreService.getPlayers().subscribe((data: PlayerProfile[]) => {
+      console.log("Datos de jugadores obtenidos:", data);
       this.players = data;
-    } else {
-      console.log("No se encontraron jugadores.");
-    }
-  }, error => {
-    console.error("Error al obtener los jugadores:", error);
+      this.aplicarFiltros();  // Aplica los filtros después de cargar los jugadores
+    }, error => {
+      console.error("Error al obtener los jugadores:", error);
+    });
+  }
+
+aplicarFiltros() {
+  this.jugadoresFiltrados = this.players.filter(jugador => {
+    return (
+      (this.tipoJugador === 'todos' || jugador.playerType === this.tipoJugador) &&
+      (this.piernaHabil === 'ambas' || jugador.footPreference === this.piernaHabil) &&
+      (this.posicion === 'todos' || jugador.position === this.posicion) &&
+      (!this.edad || jugador.age >= this.edad) &&
+      (!this.altura || jugador.height >= this.altura) &&
+      (!this.experiencia || jugador.yearsOfExperience >= this.experiencia) &&
+      (!this.pais || jugador.country.toLowerCase().includes(this.pais.toLowerCase())) &&
+      (!this.provincia || jugador.province.toLowerCase().includes(this.provincia.toLowerCase())) &&
+      (!this.buscador || jugador.playerName.toLowerCase().includes(this.buscador.toLowerCase()))
+    );
   });
+  console.log("Jugadores filtrados:", this.jugadoresFiltrados);
 }
 
 
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
